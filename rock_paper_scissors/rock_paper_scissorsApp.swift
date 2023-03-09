8b3215bd2a72659d37df5d3b8f46cbf01ac9c8e3
David Olagookun <erijesudo@gmail.com>
2023-03-09 05:29:02 +0100

    Initial Commit

---
+++ rock_paper_scissors/rock_paper_scissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  rock_paper_scissorsApp.swift
+//  rock_paper_scissors
+//
+//  Created by David OH on 09/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct rock_paper_scissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
